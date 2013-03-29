380caea808f7f41d3c39d1a5f818edf516b961f8
P.Kernevez <pkernevez@octo.com>
2013-03-29 20:24:54 +0100

    Maven Ok

---
+++ src/test/java/com/octo/techclub/memoryleak/TestLeak.java
@@ -0,0 +1,14 @@
+package com.octo.techclub.memoryleak;
+
+import junit.framework.Assert;
+
+
+public class TestLeak {
+
+//	@Test
+	public void testMyLoadClass()
+	 throws Exception {
+//		Assert.
+	}
+
+}
